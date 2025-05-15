7237487ab45759df1000d56f9224511dc7ac26f1
andiaye33 <a.ndiaye@catie.fr>
2025-05-15 11:17:14 +0200

    add JSON config parsing

--- src/main.py
+++ src/main.py
@@ -1,10 +1,31 @@
 import json
+import pprint
 
 config_path = "config/config.json"
+output = []
 
+# Load json
 with open(config_path, "r", encoding="utf-8") as file:
     config = json.load(file)
 
-print("Configuration input:")
-for key, value in config.items():
-    print(f"{key} : {value}\n")
+for device in config["devices"]:
+    device_name = device["name"]
+    
+    frame_size = device["communication"]["frames"][0]["size"]
+    
+    for frame in device["communication"]["frames"]:
+        for field in frame["data"]:
+            entry = {
+                "device": device_name,
+                "field": field["name"],
+                "msb": field["msb"],
+                "lsb": field["lsb"],
+                "bytes_used": abs(field["msb"] - field["lsb"]) + 1,
+                "endianness": "big" if field["msb"] > field["lsb"] else "little",
+                "valid_range": field["msb"] < frame_size and field["lsb"] < frame_size
+            }
+            output.append(entry)
+
+# Display 
+print("Configuration input:\n")
+pprint.pprint(output)
